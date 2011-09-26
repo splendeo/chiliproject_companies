c57bec6c01d0dd945fb8e7e2800891f8f589b4f0
Enrique García Cota <kikito@gmail.com>
2011-09-26 16:30:20 +0200

    added i18n for companies views

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -1,12 +1,5 @@
 module CompaniesHelper
 
-  def render_projects_sentence(company)
-    projects = company.projects.visible.all(:order => 'name ASC')
-    links = projects.collect{ |project| link_to_project project }
-
-    return links.to_sentence(:last_word_connector => ' and ')
-  end
-
   def logo_of(company)
     if company.logo_file_exists?
       url = logo_url(company.logo)
